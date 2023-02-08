e8f5beb43acfdd59aaf2cb40ad8e2205e8430df3
SergiuA17 <98698995+SergiuLA17@users.noreply.github.com>
2023-02-08 13:10:23 +0200

    did some tests

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,34 @@
-package org.example;public class Main {
+package org.example;
+//Spark este compatibil cu Java 11
+//Pentru a rula programul fara probleme trebuie sa adaugam in VM options:
+//--add-exports java.base/sun.nio.ch=ALL-UNNAMED
+
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.Arrays;
+
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        SparkSession spark = SparkSession.builder().appName("Read Parquet File").master("local").getOrCreate();
+//
+//        Dataset<Row> dataFrame = spark.read().parquet("/Users/segio/Desktop/License/src/main/resources/parquet/andorra_28-09-2022.osm.pbf.node.parquet");
+//
+//        dataFrame.show();
+
+        SparkConf conf = new SparkConf().setAppName("Read Parquet File").setMaster("local");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<Integer> rdd = sc.parallelize(Arrays.asList(1,2,3));
+        JavaRDD<Integer> filtered = rdd.filter(x -> x == 2);
+
+        System.out.println(filtered.first());
+
     }
 }
